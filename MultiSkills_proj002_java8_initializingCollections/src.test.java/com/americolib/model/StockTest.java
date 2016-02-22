dea0ec5b2ed98680a6ed2dc3d714496188267020
atafs <atafs@iscte-iul.pt>
2016-02-22 01:28:21 +0000

    continue

---
+++ MultiSkills_proj002_java8_initializingCollections/src.test.java/com/americolib/model/StockTest.java
@@ -0,0 +1,18 @@
+/** @author AmericoTomas */
+
+package com.americolib.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StockTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
+
+
